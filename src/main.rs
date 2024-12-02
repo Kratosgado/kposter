448b25446b9299c2a8146fbc366e68d00805bbf6
Kratosgado <mbeahessilfieprince@gmail.com>
2024-12-02 16:37:12 +0000

    minor

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use headless_chrome::{Browser, LaunchOptions};
 use kposter::auth::x::login::{login, LoginDto};
 
 use kposter::create::post::post;
@@ -9,4 +10,25 @@ async fn main() {
         password: "what".to_string(),
     });
     post("hello".to_string()).await;
+}
+
+fn launch_browser() -> Result<Browser, Box<dyn std::error::Error>> {
+    let browser = Browser::new(
+        LaunchOptions::default_builder()
+            .headless(true)
+            .build()
+            .unwrap(),
+    );
+    Ok(browser)
+}
+
+async fn login_twitter(username: &str, password: &str) -> Result<(), Box<dyn std::error::Error>> {
+    // launch browser
+    let browser = launch_browser()?;
+    let tab = browser.new_tab()?;
+
+    // open x login page
+    tab.navigate_to("https://web.facebook.com/login.php/?_rdc=1&_rdr")?
+        .await?;
+    tab.wait_until_navigated()?;
 }
